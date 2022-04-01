3de3716cad8116485bce56a6ff3ddc6924695c89
Robin9800 <robin.costanzo@gmail.com>
2022-04-01 12:45:07 +0200

    getlastDate  fixing

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const contacts = [
         visible: true,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: '15:30',
                 message: 'Hai portato a spasso il cane?',
                 status: 'sent'
             },
             {
-                date: '10/01/2020 15:50:00',
+                date: '15:50',
                 message: 'Ricordati di stendere i panni',
                 status: 'sent'
             },
             {
-                date: '10/01/2020 16:15:22',
+                date: '16:15',
                 message: 'Tutto fatto!',
                 status: 'received'
             }
@@ -27,17 +27,17 @@ const contacts = [
         visible: true,
         messages: [
             {
-                date: '20/03/2020 16:30:00',
+                date: '16:30',
                 message: 'Ciao come stai?',
                 status: 'sent'
             },
             {
-                date: '20/03/2020 16:30:55',
+                date: '16:30',
                 message: 'Bene grazie! Stasera ci vediamo?',
                 status: 'received'
             },
             {
-                date: '20/03/2020 16:35:00',
+                date: '16:35',
                 message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                 status: 'sent'
             }
@@ -49,17 +49,17 @@ const contacts = [
         visible: true,
         messages: [
             {
-                date: '28/03/2020 10:10:40',
+                date: '10:10',
                 message: 'La Marianna va in campagna',
                 status: 'received'
             },
             {
-                date: '28/03/2020 10:20:10',
+                date: '10:20',
                 message: 'Sicuro di non aver sbagliato chat?',
                 status: 'sent'
             },
             {
-                date: '28/03/2020 16:15:22',
+                date: '16:15',
                 message: 'Ah scusa!',
                 status: 'received'
             }
@@ -71,12 +71,12 @@ const contacts = [
         visible: true,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: '15:30',
                 message: 'Lo sai che ha aperto una nuova pizzeria?',
                 status: 'sent'
             },
             {
-                date: '10/01/2020 15:50:00',
+                date: '15:50',
                 message: 'Si, ma preferirei andare al cinema',
                 status: 'received'
             }
@@ -88,12 +88,12 @@ const contacts = [
         visible: true,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: '15:30',
                 message: 'Ricordati di chiamare la nonna',
                 status: 'sent'
             },
             {
-                date: '10/01/2020 15:50:00',
+                date: '15:50',
                 message: 'Va bene, stasera la sento',
                 status: 'received'
             }
@@ -105,17 +105,17 @@ const contacts = [
         visible: true,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: '15:30',
                 message: 'Ciao Claudia, hai novità?',
                 status: 'sent'
             },
             {
-                date: '10/01/2020 15:50:00',
+                date: '15:50',
                 message: 'Non ancora',
                 status: 'received'
             },
             {
-                date: '10/01/2020 15:51:00',
+                date: '15:51',
                 message: 'Nessuna nuova, buona nuova',
                 status: 'sent'
             }
@@ -127,12 +127,12 @@ const contacts = [
         visible: true,
         messages: [
             {
-                date: '10/01/2020 15:30:55',
+                date: '15:30',
                 message: 'Fai gli auguri a Martina che è il suo compleanno!',
                 status: 'sent'
             },
             {
-                date: '10/01/2020 15:50:00',
+                date: '15:50',
                 message: 'Grazie per avermelo ricordato, le scrivo subito!',
                 status: 'received'
             }
@@ -157,12 +157,11 @@ const app = new Vue({
         setContactActive(contact){
             this.contactActive = contact
         },
+        //Creo una funzione per far apparire nella contact list l'ultima data
         getLastDate(contact){
-            const lastMessage = this.getLastMessage(contact);
-            const date = lastMessage.date
-            const fullhour = date.split(' ')[1];
-            const temporary = fullhour.split(':');
-            return temporary[0] + ':' + temporary[1];
-        }
+            const messages = contact.messages;
+            const lastDate = messages[messages.length-1].date;
+            return lastDate;
+        },
     }
 })
